c1e8bc7b758979a76b0f9568ef305d65e6f0f3d4
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-01-22 10:04:19 +0000

    Bump version: 0.1.31 → 0.1.32

--- domdf_wxpython_tools/__init__.py
+++ domdf_wxpython_tools/__init__.py
@@ -43,7 +43,7 @@ __author__ = "Dominic Davis-Foster"
 __copyright__ = "Copyright 2019 Dominic Davis-Foster"
 
 __license__ = "LGPL"
-__version__ = "0.1.31"
+__version__ = "0.1.32"
 __email__ = "dominic@davis-foster.co.uk"
 
 from . import dialogs
