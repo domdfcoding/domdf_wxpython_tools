4348ab7a56b99c2459579414242263278c86bfa0
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-13 22:45:16 +0000

    Updated config files.

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -1,5 +1,5 @@
-#  This file is managed by `git_helper`. Don't edit it directly
-#  Copyright (C) 2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+#  This file is managed by 'repo_helper'. Don't edit it directly.
+#  Copyright © 2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
 #
 #  This file is distributed under the same license terms as the program it came with.
 #  There will probably be a file called LICEN[S/C]E in the same directory as this file.
@@ -17,25 +17,9 @@ import pathlib
 __all__ = [
 		"__copyright__",
 		"__version__",
-		"modname",
-		"pypi_name",
-		"py_modules",
-		"entry_points",
-		"__license__",
-		"short_desc",
-		"author",
-		"author_email",
-		"github_username",
-		"web",
-		"github_url",
-		"project_urls",
 		"repo_root",
-		"long_description",
 		"install_requires",
 		"extras_require",
-		"classifiers",
-		"keywords",
-		"import_name",
 		]
 
 __copyright__ = """
@@ -44,50 +28,6 @@ __copyright__ = """
 
 __version__ = "0.2.5"
 
-modname = "domdf_wxpython_tools"
-pypi_name = "domdf_wxpython_tools"
-import_name = "domdf_wxpython_tools"
-py_modules = []
-entry_points = {
-		"console_scripts": []
-		}
-
-__license__ = "GNU Lesser General Public License v3 or later (LGPLv3+)"
-
-short_desc = "Tools and widgets for wxPython."
-
-__author__ = author = "Dominic Davis-Foster"
-author_email = "dominic@davis-foster.co.uk"
-github_username = "domdfcoding"
-web = github_url = f"https://github.com/domdfcoding/domdf_wxpython_tools"
-project_urls = {
-		"Documentation": f"https://domdf_wxpython_tools.readthedocs.io",
-		"Issue Tracker": f"{github_url}/issues",
-		"Source Code": github_url,
-		}
-
 repo_root = pathlib.Path(__file__).parent
-
-# Get info from files; set: long_description
-long_description = (repo_root / "README.rst").read_text().replace("0.2.5", __version__) + '\n'
-
-install_requires = (repo_root / "requirements.txt").read_text().split('\n')
+install_requires = (repo_root / "requirements.txt").read_text(encoding="utf-8").split('\n')
 extras_require = {'all': []}
-
-classifiers = [
-		'Development Status :: 4 - Beta',
-		'Intended Audience :: Developers',
-		'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python',
-		'Programming Language :: Python :: 3.6',
-		'Programming Language :: Python :: 3.7',
-		'Programming Language :: Python :: 3.8',
-		'Topic :: Software Development :: Libraries :: Python Modules',
-		'Programming Language :: Python :: Implementation :: CPython',
-		'Programming Language :: Python :: 3 :: Only',
-		'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
-
-		]
-
-keywords = ""
--- doc-source/conf.py
+++ doc-source/conf.py
@@ -1,32 +1,25 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
-# This file is managed by `git_helper`. Don't edit it directly
+# This file is managed by 'repo_helper'. Don't edit it directly.
 
 # stdlib
 import os
 import re
 import sys
-import warnings
-from sphinx.locale import _
-
-# Suppress warnings from sphinx_autodoc_typehints
-# TODO: Remove once the following issues is resolved:
-# https://github.com/agronholm/sphinx-autodoc-typehints/issues/133
-warnings.filterwarnings('ignore', message='sphinx.util.inspect.Signature\(\) is deprecated')
 
 sys.path.append(os.path.abspath('.'))
 sys.path.append(os.path.abspath('..'))
 
+# this package
 from __pkginfo__ import __version__
 
-
-github_url = f"https://github.com/domdfcoding/domdf_wxpython_tools"
+github_username = "domdfcoding"
+github_repository = "domdf_wxpython_tools"
+github_url = f"https://github.com/{github_username}/{github_repository}"
 
 rst_prolog = f""".. |pkgname| replace:: domdf_wxpython_tools
 .. |pkgname2| replace:: ``domdf_wxpython_tools``
 .. |browse_github| replace:: `Browse the GitHub Repository <{github_url}>`__
-.. |ghurl| replace:: {github_url}
 """
 
 author = "Dominic Davis-Foster"
@@ -38,22 +31,26 @@ language = 'en'
 package_root = "domdf_wxpython_tools"
 
 extensions = [
+		'sphinx_toolbox',
+		'sphinx_toolbox.more_autodoc',
+		'sphinx_toolbox.more_autosummary',
+		'sphinx_toolbox.tweaks.param_dash',
 		'sphinx.ext.intersphinx',
-		'sphinx.ext.autodoc',
 		'sphinx.ext.mathjax',
-		'sphinx.ext.viewcode',
 		'sphinxcontrib.httpdomain',
-		"sphinxcontrib.extras_require",
-		"sphinx.ext.todo",
-		"sphinxemoji.sphinxemoji",
-		"notfound.extension",
-		"sphinx_tabs.tabs",
-		"sphinx-prompt",
-		"sphinx_autodoc_typehints",
+		'sphinxcontrib.extras_require',
+		'sphinx.ext.todo',
+		'sphinxemoji.sphinxemoji',
+		'notfound.extension',
+		'sphinx_copybutton',
+		'sphinxcontrib.default_values',
+		'sphinxcontrib.toctree_plus',
+		'seed_intersphinx_mapping',
 		]
 
 sphinxemoji_style = 'twemoji'
-todo_include_todos = bool(os.environ.get("SHOW_TODOS", False))
+todo_include_todos = bool(os.environ.get("SHOW_TODOS", 0))
+gitstamp_fmt = "%d %b %Y"
 
 templates_path = ['_templates']
 html_static_path = ['_static']
@@ -65,73 +62,76 @@ suppress_warnings = ['image.nonlocal_uri']
 pygments_style = 'default'
 
 intersphinx_mapping = {
-		'rtd': ('https://docs.readthedocs.io/en/latest/', None),
-		'sphinx': ('https://www.sphinx-doc.org/en/stable/', None),
 		'python': ('https://docs.python.org/3/', None),
-		"NumPy": ('https://numpy.org/doc/stable/', None),
-		"SciPy": ('https://docs.scipy.org/doc/scipy/reference', None),
-		"matplotlib": ('https://matplotlib.org', None),
-		"h5py": ('https://docs.h5py.org/en/latest/', None),
-		"Sphinx": ('https://www.sphinx-doc.org/en/stable/', None),
-		"Django": ('https://docs.djangoproject.com/en/dev/', 'https://docs.djangoproject.com/en/dev/_objects/'),
-		"sarge": ('https://sarge.readthedocs.io/en/latest/', None),
-		"attrs": ('https://www.attrs.org/en/stable/', None),
+		'sphinx': ('https://www.sphinx-doc.org/en/stable/', None),
 		}
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'domdf_sphinx_theme'
 html_theme_options = {
 		'logo_only': False,
 		}
 html_theme_path = ["../.."]
-# html_logo = "logo/pyms.png"
-html_show_sourcelink = False  # True will show link to source
+html_show_sourcelink = True  # True will show link to source
 
 html_context = {
 		'display_github': True,
 		'github_user': 'domdfcoding',
 		'github_repo': 'domdf_wxpython_tools',
 		'github_version': 'master',
-		'conf_py_path': '/',
+		'conf_py_path': '/doc-source/',
 		}
 
 htmlhelp_basename = slug
 
-latex_documents = [
-		('index', f'{slug}.tex', project, author, 'manual'),
+latex_documents = [('index', f'{slug}.tex', project, author, 'manual')]
+man_pages = [('index', slug, project, [author], 1)]
+texinfo_documents = [('index', slug, project, author, slug, project, 'Miscellaneous')]
+
+toctree_plus_types = {
+		"class",
+		"function",
+		"method",
+		"data",
+		"enum",
+		"flag",
+		"confval",
+		"directive",
+		"role",
+		"confval",
+		"protocol",
+		"typeddict",
+		"namedtuple",
+		"exception",
+		}
+
+add_module_names = False
+hide_none_rtype = True
+all_typevars = True
+overloads_location = "bottom"
+
+
+autodoc_exclude_members = [   # Exclude "standard" methods.
+		"__dict__",
+		"__class__",
+		"__dir__",
+		"__weakref__",
+		"__module__",
+		"__annotations__",
+		"__orig_bases__",
+		"__parameters__",
+		"__subclasshook__",
+		"__init_subclass__",
+		"__attrs_attrs__",
+		"__init__",
+		"__new__",
+		"__getnewargs__",
+		"__abstractmethods__",
+		"__hash__",
 		]
-
-man_pages = [
-		('index', slug, project, [author], 1)
-		]
-
-texinfo_documents = [
-		('index', slug, project, author, slug, project, 'Miscellaneous'),
-		]
-
-
-# Extensions to theme docs
-def setup(app):
-	from sphinx.domains.python import PyField
-	from sphinx.util.docfields import Field
-
-	app.add_object_type(
-			'confval',
-			'confval',
-			objname='configuration value',
-			indextemplate='pair: %s; configuration value',
-			doc_field_types=[
-					PyField(
-							'type',
-							label=_('Type'),
-							has_arg=False,
-							names=('type',),
-							bodyrolename='class'
-							),
-					Field(
-							'default',
-							label=_('Default'),
-							has_arg=False,
-							names=('default',),
-							),
-					]
-			)
+autodoc_default_options = {
+		'members': None,  # Include all members (methods).
+		'special-members': None,
+		"autosummary": None,
+		"show-inheritance": None,
+		'exclude-members': ','.join(autodoc_exclude_members),
+		}
